83add4b56fec4080de82ad883f5d053d35bab588
PrivateChpok <sazanov04@bk.ru>
2023-03-09 08:54:50 +0200

    comments

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,5 +1,4 @@
 let updateBtns = document.getElementsByClassName('update-cart')
-
 for (i = 0; i < updateBtns.length; i++) {
 	updateBtns[i].addEventListener('click', function(){
 		let productId = this.dataset.product
